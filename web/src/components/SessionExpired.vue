d7cc3870ad9b49ee515b9069a85586462328188a
wh1te909 <dcparsi@gmail.com>
2019-12-15 05:23:08 +0000

    force page reload when session expires to pull latest version of frontend

--- web/src/components/SessionExpired.vue
+++ web/src/components/SessionExpired.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="fixed-center text-center">
     <p class="text-faded">Your session has expired</p>
-    <q-btn
-      color="secondary"
-      style="width:200px;"
-      @click="$router.push('/')"
-    >Login</q-btn>
+    <q-btn color="secondary" style="width:200px;" @click="$router.push('/')">Login</q-btn>
   </div>
 </template>
 
 <script>
 export default {
   name: "SessionExpired",
-  mounted() {
+  created() {
     this.$store.dispatch("destroyToken");
+  },
+  mounted() {
+    if (localStorage.getItem("reloaded")) {
+      localStorage.removeItem("reloaded");
+    } else {
+      localStorage.setItem("reloaded", "1");
+      location.reload();
+    }
   }
-}
+};
 </script>

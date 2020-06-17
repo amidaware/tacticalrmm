b514f92d5108397768d471ce98bb685f64c7aa19
wh1te909 <dcparsi@gmail.com>
2020-06-17 07:26:47 +0000

    add test

--- api/tacticalrmm/checks/tests.py
+++ api/tacticalrmm/checks/tests.py
@@ -19,7 +19,7 @@ class TestCheckViews(BaseTestCase):
     def test_add_disk_check(self):
         url = "/checks/checks/"
 
-        self.valid_payload = {
+        valid_payload = {
             "pk": self.agent.pk,
             "check": {
                 "check_type": "diskspace",
@@ -29,11 +29,11 @@ class TestCheckViews(BaseTestCase):
             },
         }
 
-        resp = self.client.post(url, self.valid_payload, format="json")
+        resp = self.client.post(url, valid_payload, format="json")
         self.assertEqual(resp.status_code, 200)
 
         # this should fail because we already have a check for drive C: in setup
-        self.invalid_payload = {
+        invalid_payload = {
             "pk": self.agent.pk,
             "check": {
                 "check_type": "diskspace",
@@ -43,7 +43,7 @@ class TestCheckViews(BaseTestCase):
             },
         }
 
-        resp = self.client.post(url, self.invalid_payload, format="json")
+        resp = self.client.post(url, invalid_payload, format="json")
         self.assertEqual(resp.status_code, 400)
 
     def test_get_policy_disk_check(self):
@@ -59,7 +59,7 @@ class TestCheckViews(BaseTestCase):
     def test_add_policy_disk_check(self):
         url = "/checks/checks/"
 
-        self.valid_payload = {
+        valid_payload = {
             "policy": self.policy.pk,
             "check": {
                 "check_type": "diskspace",
@@ -69,11 +69,11 @@ class TestCheckViews(BaseTestCase):
             },
         }
 
-        resp = self.client.post(url, self.valid_payload, format="json")
+        resp = self.client.post(url, valid_payload, format="json")
         self.assertEqual(resp.status_code, 200)
 
         # this should fail because we already have a check for drive M: in setup
-        self.invalid_payload = {
+        invalid_payload = {
             "policy": self.policy.pk,
             "check": {
                 "check_type": "diskspace",
@@ -83,7 +83,7 @@ class TestCheckViews(BaseTestCase):
             },
         }
 
-        resp = self.client.post(url, self.invalid_payload, format="json")
+        resp = self.client.post(url, invalid_payload, format="json")
         self.assertEqual(resp.status_code, 400)
 
     def test_get_disks_for_policies(self):
@@ -91,3 +91,27 @@ class TestCheckViews(BaseTestCase):
         r = self.client.get(url)
         self.assertIsInstance(r.data, list)
         self.assertEqual(26, len(r.data))
+    
+
+    def test_edit_check_alert(self):
+        url_a = f"/checks/{self.agentDiskCheck.pk}/check/"
+        url_p = f"/checks/{self.policyDiskCheck.pk}/check/"
+
+        valid_payload = {"email_alert": False, "check_alert": True}
+        invalid_payload = {"email_alert": False}
+
+        with self.assertRaises(KeyError) as err:
+            resp = self.client.patch(url_a, invalid_payload, format="json")
+        
+        with self.assertRaises(KeyError) as err:
+            resp = self.client.patch(url_p, invalid_payload, format="json")
+        
+        resp = self.client.patch(url_a, valid_payload, format="json")
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.patch(url_p, valid_payload, format="json")
+        self.assertEqual(resp.status_code, 200)
+
+        self.check_not_authenticated("patch", url_a)
+
+        

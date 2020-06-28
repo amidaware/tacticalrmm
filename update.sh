1675f863e1be2c70b646fb88627c46ed31c52d0f
wh1te909 <dcparsi@gmail.com>
2020-06-28 05:18:31 +0000

    fix update script

--- update.sh
+++ update.sh
@@ -5,17 +5,17 @@ do
 sudo systemctl stop ${i}
 done
 
-cd /home/${USER}/rmm/
+cd /rmm
 git fetch origin develop
 git reset --hard FETCH_HEAD
 git clean -df
-cp /home/${USER}/rmm/_modules/* /srv/salt/_modules/
-cp /home/${USER}/rmm/scripts/* /srv/salt/scripts/
-rm -rf /home/${USER}/rmm/api/env
-cd /home/${USER}/rmm/api
-python3.7 -m venv env
-source /home/${USER}/rmm/api/env/bin/activate
-cd /home/${USER}/rmm/api/tacticalrmm
+cp /rmm/_modules/* /srv/salt/_modules/
+cp /rmm/scripts/* /srv/salt/scripts/
+rm -rf /rmm/api/env
+cd /rmm/api
+python3 -m venv env
+source /rmm/api/env/bin/activate
+cd /rmm/api/tacticalrmm
 pip install --no-cache-dir --upgrade pip
 pip install --no-cache-dir --upgrade setuptools wheel
 pip install --no-cache-dir -r requirements.txt
@@ -25,13 +25,13 @@ python manage.py delete_tokens
 deactivate
 
 
-rm -rf /home/${USER}/rmm/web/node_modules
-rm -rf /home/${USER}/rmm/web/dist
-cd /home/${USER}/rmm/web
+rm -rf /rmm/web/node_modules
+rm -rf /rmm/web/dist
+cd /rmm/web
 npm install
 npm run build
 sudo rm -rf /var/www/rmm/dist
-sudo cp -pvr /home/${USER}/rmm/web/dist /var/www/rmm/
+sudo cp -pvr /rmm/web/dist /var/www/rmm/
 sudo chown www-data:www-data -R /var/www/rmm/dist
 
 for i in celery celery-winupdate celerybeat rmm nginx

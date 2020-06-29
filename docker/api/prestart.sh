b06d2e0be8980c8be323d94e0efea7786c57e3ac
Josh Krawczyk <josh@torchlake.com>
2020-06-29 18:36:48 -0400

    Finish up MeshCentral integration. Worked out more bugs and added mesh settings in CoreSettings. Modified docker to work with new configurations

--- docker/api/prestart.sh
+++ docker/api/prestart.sh
@@ -4,4 +4,5 @@ sleep 5
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 python manage.py initial_db_setup
+python manage.py initial_mesh_setup
 python manage.py load_chocos

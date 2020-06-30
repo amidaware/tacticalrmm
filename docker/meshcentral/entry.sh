98ca5e5c5548a38b2ecf527903937347a7431d26
wh1te909 <dcparsi@gmail.com>
2020-06-30 08:00:38 +0000

    fix email_user var in docker

--- docker/meshcentral/entry.sh
+++ docker/meshcentral/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-node node_modules/meshcentral --createaccount ${MESH_USER} --pass ${MESH_PASS} --email ${EMAIL}
+node node_modules/meshcentral --createaccount ${MESH_USER} --pass ${MESH_PASS} --email ${EMAIL_USER}
 node node_modules/meshcentral --adminaccount ${MESH_USER}
 
 FILE=/token/token.key

98dba0dece55738a9240a0a878cad8af16b56cfb
Josh Krawczyk <josh@simplermm.com>
2020-08-18 13:00:45 -0400

    alerts work

--- api/tacticalrmm/alerts/admin.py
+++ api/tacticalrmm/alerts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Alert
+
+
+admin.site.register(Alert)
--- web/src/mixins/mixins.js
+++ web/src/mixins/mixins.js
@@ -32,30 +32,41 @@ export function notifyInfoConfig(msg, timeout = 2000) {
   }
 };
 
+function getTimeLapse(unixtime) {
+  var previous = unixtime * 1000;
+  var current = new Date();
+  var msPerMinute = 60 * 1000;
+  var msPerHour = msPerMinute * 60;
+  var msPerDay = msPerHour * 24;
+  var msPerMonth = msPerDay * 30;
+  var msPerYear = msPerDay * 365;
+  var elapsed = current - previous;
+  if (elapsed < msPerMinute) {
+    return Math.round(elapsed / 1000) + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    return Math.round(elapsed / msPerYear) + " years ago";
+  }
+}
+
 export default {
   methods: {
     bootTime(unixtime) {
-      var previous = unixtime * 1000;
-      var current = new Date();
-      var msPerMinute = 60 * 1000;
-      var msPerHour = msPerMinute * 60;
-      var msPerDay = msPerHour * 24;
-      var msPerMonth = msPerDay * 30;
-      var msPerYear = msPerDay * 365;
-      var elapsed = current - previous;
-      if (elapsed < msPerMinute) {
-        return Math.round(elapsed / 1000) + " seconds ago";
-      } else if (elapsed < msPerHour) {
-        return Math.round(elapsed / msPerMinute) + " minutes ago";
-      } else if (elapsed < msPerDay) {
-        return Math.round(elapsed / msPerHour) + " hours ago";
-      } else if (elapsed < msPerMonth) {
-        return Math.round(elapsed / msPerDay) + " days ago";
-      } else if (elapsed < msPerYear) {
-        return Math.round(elapsed / msPerMonth) + " months ago";
-      } else {
-        return Math.round(elapsed / msPerYear) + " years ago";
-      }
+      return getTimeLapse(unixtime)
+    },
+    alertTime(datetime) {
+      console.log(datetime)
+      var unixtime = new Date(datetime).getTime()/1000
+      console.log(unixtime)
+      return getTimeLapse(parseInt(unixtime))
+
     },
     notifySuccess(msg, timeout = 2000) {
       Notify.create(notifySuccessConfig(msg, timeout));

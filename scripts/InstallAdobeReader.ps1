094c6df4a7f6e6b228933598e69d81cb21a936a2
Omnicef <69552619+Omnicef@users.noreply.github.com>
2020-08-25 18:53:32 -0700

    Update InstallAdobeReader.ps1

--- scripts/InstallAdobeReader.ps1
+++ scripts/InstallAdobeReader.ps1
@@ -1,2 +1,2 @@
 ﻿#Install Adobe Reader DC
-choco install adobereader -params '"/EnableUpdateService /UpdateMode:3"' --yes --no-progress
+choco install adobereader -params '"/EnableUpdateService /UpdateMode:3 /DesktopIcon"' --yes --no-progress --force

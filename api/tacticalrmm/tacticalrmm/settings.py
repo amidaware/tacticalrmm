bd43b5e16275e3a4bbd289a72b7132d46b06201c
wh1te909 <dcparsi@gmail.com>
2020-10-27 04:43:04 +0000

    agent 1.0.1

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -16,7 +16,7 @@ APP_VER = "0.0.81"
 # https://github.com/wh1te909/salt
 LATEST_SALT_VER = "1.1.0"
 
-LATEST_AGENT_VER = "1.0.0"
+LATEST_AGENT_VER = "1.0.1"
 
 DL_64 = f"https://github.com/wh1te909/rmmagent/releases/download/v{LATEST_AGENT_VER}/winagent-v{LATEST_AGENT_VER}.exe"
 DL_32 = f"https://github.com/wh1te909/rmmagent/releases/download/v{LATEST_AGENT_VER}/winagent-v{LATEST_AGENT_VER}-x86.exe"

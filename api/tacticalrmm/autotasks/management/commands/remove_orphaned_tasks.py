e09ad7ed23693d390778edad5b1d2364892f620b
wh1te909 <dcparsi@gmail.com>
2020-10-31 08:06:50 +0000

    only send task to online agents

--- api/tacticalrmm/autotasks/management/commands/remove_orphaned_tasks.py
+++ api/tacticalrmm/autotasks/management/commands/remove_orphaned_tasks.py
@@ -1,5 +1,4 @@
 from django.core.management.base import BaseCommand
-from django.conf import settings
 from agents.models import Agent
 from autotasks.tasks import remove_orphaned_win_tasks
 
@@ -8,10 +7,13 @@ class Command(BaseCommand):
     help = "Checks for orphaned tasks on all agents and removes them"
 
     def handle(self, *args, **kwargs):
-
-        for agent in Agent.objects.all():
+        agents = Agent.objects.all()
+        online = [i for i in agents if i.status == "online"]
+        for agent in online:
             remove_orphaned_win_tasks.delay(agent.pk)
 
         self.stdout.write(
-            "The task has been initiated. Check the Debug Log in the UI for progress."
+            self.style.SUCCESS(
+                "The task has been initiated. Check the Debug Log in the UI for progress."
+            )
         )

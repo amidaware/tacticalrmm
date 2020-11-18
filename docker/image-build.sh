7592c11e99ca3998c85ca0c5ea15a0b5e56bf6d7
sadnub <josh@torchlake.com>
2020-11-17 23:39:03 -0500

    more docker changes

--- docker/image-build.sh
+++ docker/image-build.sh
@@ -5,12 +5,11 @@
 set -o errexit
 set -o pipefail
 
-#  tactical-frontend tactical-nginx tactical-salt --no-cache
-DOCKER_IMAGES="tactical"
+DOCKER_IMAGES="tactical tactical-meshcentral tactical-frontend tactical-nginx tactical-salt"
 
 cd ..
 
 for DOCKER_IMAGE in ${DOCKER_IMAGES}; do
   echo "Building Tactical Image: ${DOCKER_IMAGE}..."
-  docker build --pull --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t "${DOCKER_IMAGE}" -f "docker/containers/${DOCKER_IMAGE}/dockerfile" .
+  docker build --pull --no-cache --build-arg BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" -t "${DOCKER_IMAGE}" -f "docker/containers/${DOCKER_IMAGE}/dockerfile" .
 done

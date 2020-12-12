1b1eec40a7c3eb69b62e4f6a2e9dbdc45749d63b
wh1te909 <dcparsi@gmail.com>
2020-12-12 09:39:20 +0000

    agent check-in and recovery improvements

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -15,7 +15,7 @@ EXE_DIR = os.path.join(BASE_DIR, "tacticalrmm/private/exe")
 AUTH_USER_MODEL = "accounts.User"
 
 # latest release
-TRMM_VERSION = "0.2.14"
+TRMM_VERSION = "0.2.15"
 
 # bump this version everytime vue code is changed
 # to alert user they need to manually refresh their browser
@@ -25,7 +25,7 @@ APP_VER = "0.0.99"
 LATEST_SALT_VER = "1.1.0"
 
 # https://github.com/wh1te909/rmmagent
-LATEST_AGENT_VER = "1.1.9"
+LATEST_AGENT_VER = "1.1.10"
 
 MESH_VER = "0.7.14"
 

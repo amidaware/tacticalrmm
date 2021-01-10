1aa48c6d62836ff5c77a4db834858db2549c6c99
dinger1986 <daniel.lamb@flonix.co.uk>
2021-01-10 17:42:11 +0000

    Install OpenSSH on PCs

--- scripts/OpenSSHServerInstall.ps1
+++ scripts/OpenSSHServerInstall.ps1
@@ -1,13 +1,13 @@
 ##Check if openssh is installed
 if((Get-WindowsCapability -Online | ? Name -like OpenSSH*).State -eq "Installed")
 	{
-	Write-Host "OpenSSH Server is installed."
+	Write-Output "OpenSSH Server is installed."
 	}
 
 else 
 
 		{
-	Write-Host "OpenSSH Server is NOT installed.";
+	Write-Output "OpenSSH Server is NOT installed.";
 	## Install SSH
 	Add-WindowsCapability -Online -Name "OpenSSH.Server~~~~0.0.1.0"
 

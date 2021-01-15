5ef6a14d24fb320da2969e81b3530f6b582a14b5
wh1te909 <dcparsi@gmail.com>
2021-01-15 18:21:25 +0000

    add nats-api binary

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/wh1te909/tacticalrmm/natsapi"
 )
 
+var version = "1.0.0"
+
 func main() {
+	ver := flag.Bool("version", false, "Prints version")
 	apiHost := flag.String("api-host", "", "django api base url")
 	debug := flag.Bool("debug", false, "Debug")
 	flag.Parse()
+
+	if *ver {
+		fmt.Println(version)
+		return
+	}
 
 	api.Listen(*apiHost, *debug)
 }

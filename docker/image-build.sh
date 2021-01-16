278ea2478686109168c68296430ad74a21d89782
sadnub <josh@torchlake.com>
2021-01-16 11:20:24 -0500

    improve dev env

--- docker/image-build.sh
+++ docker/image-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o pipefail
 
-DOCKER_IMAGES="tactical tactical-frontend tactical-nginx tactical-meshcentral tactical-salt tactical-nats"
+DOCKER_IMAGES="tactical-nginx"
 
 cd ..
 

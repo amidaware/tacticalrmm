d9b3c19725b27eddea1a5f94c92345bc4889306c
bradhawkins85 <bradhawkins@gmail.com>
2021-02-16 16:16:41 +1000

    Update installer.ps1
    Find the latest version of winagent from github and download it instead of hard-coded version.

--- api/tacticalrmm/core/installer.ps1
+++ api/tacticalrmm/core/installer.ps1
@@ -1,5 +1,4 @@
 # author: https://github.com/bradhawkins85
-$innosetup = 'innosetupchange'
 $api = '"apichange"'
 $clientid = 'clientchange'
 $siteid = 'sitechange'
@@ -8,12 +7,28 @@ $power = powerchange
 $rdp = rdpchange
 $ping = pingchange
 $auth = '"tokenchange"'
-$downloadlink = 'downloadchange'
 
 $serviceName = 'tacticalagent'
 If (Get-Service $serviceName -ErrorAction SilentlyContinue) {
     write-host ('Tactical RMM Is Already Installed')
 } Else {
+
+	# Download latest wh1te909/rmmagent release from github
+
+	$repo = "wh1te909/rmmagent"
+	$releases = "https://api.github.com/repos/$repo/releases"
+
+	Write-Host Determining latest release
+	$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+	$file = "winagent-$tag.exe"
+
+	$downloadlink = "https://github.com/$repo/releases/download/$tag/$file"
+	if ([Environment]::Is64BitOperatingSystem) {
+		$innosetup = "$name-$tag.exe"
+	} else {
+		$innosetup = "$name-$tag-x86.exe"
+	}
+
     $OutPath = $env:TMP
     $output = $innosetup
 

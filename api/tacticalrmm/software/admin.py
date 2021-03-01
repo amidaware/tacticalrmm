a7b285759fb660e58e90872967807f4a3fa0321c
wh1te909 <dcparsi@gmail.com>
2021-03-01 21:43:54 +0000

    delete chocolog model

--- api/tacticalrmm/software/admin.py
+++ api/tacticalrmm/software/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 
-from .models import ChocoLog, ChocoSoftware, InstalledSoftware
+from .models import ChocoSoftware, InstalledSoftware
 
 
 class ChocoAdmin(admin.ModelAdmin):
     readonly_fields = ("added",)
 
 
-class ChocoLogAdmin(admin.ModelAdmin):
-    readonly_fields = ("time",)
-
-
 admin.site.register(ChocoSoftware, ChocoAdmin)
-admin.site.register(ChocoLog, ChocoLogAdmin)
 admin.site.register(InstalledSoftware)
--- api/tacticalrmm/software/models.py
+++ api/tacticalrmm/software/models.py
@@ -11,18 +11,6 @@ class ChocoSoftware(models.Model):
         return f"{len(self.chocos)} - {self.added}"
 
 
-class ChocoLog(models.Model):
-    agent = models.ForeignKey(Agent, related_name="chocolog", on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    version = models.CharField(max_length=255)
-    message = models.TextField()
-    installed = models.BooleanField(default=False)
-    time = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.agent.hostname} | {self.name} | {self.time}"
-
-
 class InstalledSoftware(models.Model):
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
     software = models.JSONField()

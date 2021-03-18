9db497092f40304de6e724b34081d478af49a210
silversword411 <github.com@datmail.com>
2021-03-18 00:38:40 -0400

    Update Rename_Computer.ps1

--- scripts/Rename_Computer.ps1
+++ scripts/Rename_Computer.ps1
@@ -3,6 +3,11 @@
 # First Command Parameter will be new computer name
 # Second Command Parameter if yes will auto-restart computer
 
+if ($Args.Count -eq 0) {
+  Write-Output "Computer name arg is required"
+  exit 1
+}
+
 $param1=$args[0]
 $ToRestartTypeYes=$args[1]
 

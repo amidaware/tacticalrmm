6f43e61c24f3a8345494e27b0b15dc789a4dcd89
silversword411 <github.com@datmail.com>
2021-03-26 18:48:40 -0400

    Disk label v2

--- web/src/components/modals/checks/DiskSpaceCheck.vue
+++ web/src/components/modals/checks/DiskSpaceCheck.vue
@@ -51,7 +51,7 @@
           outlined
           type="number"
           v-model.number="diskcheck.run_interval"
-          label="Check run interval (s) in seconds"
+          label="Check run interval (seconds)"
           hint="Setting this will override the check run interval on the agent"
         />
       </q-card-section>

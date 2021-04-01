479b528d09beddc783a044731034407824da776f
wh1te909 <dcparsi@gmail.com>
2021-04-01 09:01:57 +0000

    more tests

--- api/tacticalrmm/tacticalrmm/tests.py
+++ api/tacticalrmm/tacticalrmm/tests.py
@@ -1,16 +1,12 @@
 from unittest.mock import mock_open, patch
 
 import requests
+from django.test import TestCase, override_settings
 
-from .test import TacticalTestCase
-from .utils import generate_winagent_exe
+from .utils import generate_winagent_exe, reload_nats
 
 
-class TestUtils(TacticalTestCase):
-    def setUp(self):
-        self.authenticate()
-        self.setup_coresettings()
-
+class TestUtils(TestCase):
     @patch("requests.post")
     @patch("__main__.__builtins__.open", new_callable=mock_open)
     def test_generate_winagent_exe_success(self, m_open, mock_post):
@@ -45,3 +41,26 @@ class TestUtils(TacticalTestCase):
             file_name="rmm-client-site-server.exe",
         )
         self.assertEqual(r.status_code, 400)
+
+    @override_settings(
+        CERT_FILE="/tmp/asdasd.pem",
+        KEY_FILE="/tmp/asds55r.pem",
+        ALLOWED_HOSTS=["api.example.com"],
+        SECRET_KEY="sekret",
+        DOCKER_BUILD=True,
+    )
+    @patch("subprocess.run")
+    def test_reload_nats_docker(self, mock_subprocess):
+        _ = reload_nats()
+
+        mock_subprocess.assert_not_called()
+
+    @override_settings(
+        ALLOWED_HOSTS=["api.example.com"],
+        SECRET_KEY="sekret",
+    )
+    @patch("subprocess.run")
+    def test_reload_nats(self, mock_subprocess):
+        _ = reload_nats()
+
+        mock_subprocess.assert_called_once()

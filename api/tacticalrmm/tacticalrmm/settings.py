b62e37307ebdfa1290935c8c4fa9fe5dbae3c7d3
wh1te909 <dcparsi@gmail.com>
2021-04-09 18:28:43 +0000

    revert meshcentral back to 0.7.93

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -24,7 +24,7 @@ APP_VER = "0.0.127"
 # https://github.com/wh1te909/rmmagent
 LATEST_AGENT_VER = "1.4.13"
 
-MESH_VER = "0.8.5"
+MESH_VER = "0.7.93"
 
 # for the update script, bump when need to recreate venv or npm install
 PIP_VER = "15"

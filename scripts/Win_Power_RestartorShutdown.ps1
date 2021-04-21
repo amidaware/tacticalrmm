18d10c9becde82ab84dccea2276f0a20c4ecb590
silversword411 <github.com@datmail.com>
2021-04-21 09:40:07 -0400

    power restart script tweaks

--- scripts/Win_Power_RestartorShutdown.ps1
+++ scripts/Win_Power_RestartorShutdown.ps1
@@ -1,11 +1,11 @@
-# This script will force the computer to finish installing updates
-# and restart. Normal restart doesn't install updates before issuing
-# a reboot command. This one does.
+# This script will force restart computer. Add command paramter: shutdown to shutdown instead
+# Normal restart doesn't install updates before issuing
 
 $param1 = $args[0]
 
-Restart-Computer -ComputerName $env:COMPUTERNAME -Force 
-
 if ($param1 -eq 'shutdown') {
     Stop-Computer -ComputerName $env:COMPUTERNAME -Force
+}
+else {
+    Restart-Computer -ComputerName $env:COMPUTERNAME -Force 
 }

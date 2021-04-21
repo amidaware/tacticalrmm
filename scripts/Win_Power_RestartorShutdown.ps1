890e430cb7e1aba83e65564bcc87c2e7a2579a36
silversword411 <github.com@datmail.com>
2021-04-21 09:29:25 -0400

    script library - merging scripts and parameterizing

--- scripts/Win_Power_RestartorShutdown.ps1
+++ scripts/Win_Power_RestartorShutdown.ps1
@@ -2,4 +2,10 @@
 # and restart. Normal restart doesn't install updates before issuing
 # a reboot command. This one does.
 
+$param1 = $args[0]
+
 Restart-Computer -ComputerName $env:COMPUTERNAME -Force 
+
+if ($param1 -eq 'shutdown') {
+    Stop-Computer -ComputerName $env:COMPUTERNAME -Force
+}

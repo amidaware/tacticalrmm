581e97423616d690de2af7441f18aa5325484d63
wh1te909 <dcparsi@gmail.com>
2021-06-17 04:36:34 +0000

    add view setting perms closes #569

--- api/tacticalrmm/core/permissions.py
+++ api/tacticalrmm/core/permissions.py
@@ -3,6 +3,11 @@ from rest_framework import permissions
 from tacticalrmm.permissions import _has_perm
 
 
+class ViewCoreSettingsPerms(permissions.BasePermission):
+    def has_permission(self, r, view):
+        return _has_perm(r, "can_view_core_settings")
+
+
 class EditCoreSettingsPerms(permissions.BasePermission):
     def has_permission(self, r, view):
         return _has_perm(r, "can_edit_core_settings")

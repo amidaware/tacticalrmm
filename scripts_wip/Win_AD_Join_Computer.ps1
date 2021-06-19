2af820de9ac646d357dcd605eed0c1262be63d49
rfost52 <85522106+rfost52@users.noreply.github.com>
2021-06-18 22:43:26 -0400

    Update Win_AD_Join_Computer.ps1
    Parameters, error checking with exit codes

--- scripts_wip/Win_AD_Join_Computer.ps1
+++ scripts_wip/Win_AD_Join_Computer.ps1
@@ -6,17 +6,62 @@
     I really do a lot of this
 
     .OUTPUTS
-    Results are printed to the console. Future releases will support outputting to a log file. 
-
+    Results are printed to the console. Future releases will support outputting to a log file.
+    
     .NOTES
     Change Log
     V1.0 Initial release
+    V1.1 Parameterization; Error Checking with conditionals and exit codes
 
     Reference Links: www.google.com
 #>
 
-$domain = "myDomain"
-$password = "myPassword!" | ConvertTo-SecureString -asPlainText -Force
-$username = "$domain\myUserAccount"
-$credential = New-Object System.Management.Automation.PSCredential($username, $password)
-Add-Computer -DomainName $domain -OUPath "OU=testOU,DC=domain,DC=Domain,DC=com" -Credential $credential -Restart
+param(
+    $domain,
+    $password,
+    $UserAccount,
+    $OUPath
+)
+
+$username = "$domain\$UserAccount"
+$credential = New-Object System.Management.Automation.PSCredential($username, $secpassword)
+
+if ([string]::IsNullOrEmpty($domain)) {
+    Write-Output "Domain must be defined. Use -domain <value> to pass it."
+    EXIT 1
+}
+
+if ([string]::IsNullOrEmpty($UserAccount)) {
+    Write-Output "User Account must be defined. Use -UserAccount <value> to pass it."
+    EXIT 1
+}
+
+if ([string]::IsNullOrEmpty($password)){
+    Write-Output "Password must be defined. Use -password <value> to pass it."
+    EXIT 1
+}
+
+else{
+    $password = ConvertTo-SecureString -string $password -asPlainText -Force
+}
+
+try{
+
+if ([string]::IsNullOrEmpty($OUPath){
+    Write-Output "OU Path is not defined. Computer object will be created in the default OU."
+    Add-Computer -DomainName $domain -Credential $credential -Restart
+    EXIT 0
+}
+
+else {
+    Add-Computer -DomainName $domain -OUPath $OUPath -Credential $credential -Restart
+    EXIT 0
+}
+}
+
+catch{
+    Write-Output "An error has occured."
+    EXIT 1
+}
+
+Exit $LASTEXITCODE

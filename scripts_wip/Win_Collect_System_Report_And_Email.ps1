eb762d4bfd517d7423652c66dccf010ab919072b
rfost52 <85522106+rfost52@users.noreply.github.com>
2021-06-24 23:53:06 -0400

    Initial Parameterization of variables

--- scripts_wip/Win_Collect_System_Report_And_Email.ps1
+++ scripts_wip/Win_Collect_System_Report_And_Email.ps1
@@ -1,12 +1,39 @@
-#The following variables should be changed:
-#$file ? should be named with a .htm ending
-#$fromaddress
-#$toaddress
-#$smtpserver
-#$Password
-#$port
+<#
+    .SYNOPSIS
+    Generates a system report in HTML format.
 
-$file = "C:\Temp\Report.htm"
+    .DESCRIPTION 
+    A report comprised of stopped services, running processes, drive space, network adapter settings, and printers is stored locally with a copy sent via e-mail
+
+    .OUTPUTS
+    Results are sent as a HTML file to C:\Temp and e-mailed based on provided parameters
+
+    .EXAMPLE
+    Example is forthcoming
+
+    .NOTES
+    Change Log
+    V1.0 Initial release and parameterization
+  
+
+    Reference Links: 
+        www.google.com
+        docs.microsoft.com
+#>
+
+
+param(
+    $fromaddress,
+    $toaddress,
+    $smtpserver,
+    $password,
+    $port,
+    $file
+)
+
+$destination = "C:\Temp\$file"
+
+
 
 #HTML Styling
 
@@ -18,7 +45,7 @@ $a = $a + "</style>"
  
 #Heading
 
-"<H1 style='color:green;'>System Report For Agent</H1>" | Out-File $file -Append
+"<H1 style='color:green;'>System Report For Agent</H1>" | Out-File $destination -Append
 
 #Network Information
 
@@ -26,7 +53,7 @@ Get-WmiObject win32_networkadapterconfiguration -filter "ipenabled = 'True'"|
 Select PSComputername, DNSHostName, Description,
 @{Name = "IPAddress";Expression = 
 {[regex]$rx = "(\d{1,3}(\.?)){4}"
-$rx.matches($_.IPAddress).Value}},MACAddress | ConvertTo-HTML -Head "<H2 style='color:green;'>Network Information</H2>" -body $a | Out-file $file -Append
+$rx.matches($_.IPAddress).Value}},MACAddress | ConvertTo-HTML -Head "<H2 style='color:green;'>Network Information</H2>" -body $a | Out-file $destination -Append
 
 #Get Event logs
 
@@ -37,29 +64,29 @@ Get-EventLog -LogName System -Newest 10 -EntryType Warning | Select TimeGenerate
 
 #Get Stopped Services
 
-Get-Service | Where {($_.Status) -eq "Stopped"} | Select Status, Name, DisplayName | ConvertTo-HTML -Head "<H2 style='color:green;'>Stopped Services</H2>" -body $a | Out-File $file -Append
+Get-Service | Where {($_.Status) -eq "Stopped"} | Select Status, Name, DisplayName | ConvertTo-HTML -Head "<H2 style='color:green;'>Stopped Services</H2>" -body $a | Out-File $destination -Append
 
 #Get Processes and CPU
 
-Get-Process | Select Id, ProcessName, CPU | ConvertTo-HTML -Head "<H2 style='color:green;'>Processes & CPU</H2>" -body $a | Out-File $file -Append
+Get-Process | Select Id, ProcessName, CPU | ConvertTo-HTML -Head "<H2 style='color:green;'>Processes & CPU</H2>" -body $a | Out-File $destination -Append
 
 #Get Mapped Drives
 
-Get-PSDrive | Where {$_.Used -ne $null} | Select Name, @{n='Used';e={[float]($_.Used/1GB)}}, @{n='Free';e={[float]($_.Free/1GB)}}, Root| ConvertTo-HTML -Head "<H2 style='color:green;'>Mapped Drives</H2>" -body $a | Out-File $file -Append
+Get-PSDrive | Where {$_.Used -ne $null} | Select Name, @{n='Used';e={[float]($_.Used/1GB)}}, @{n='Free';e={[float]($_.Free/1GB)}}, Root| ConvertTo-HTML -Head "<H2 style='color:green;'>Mapped Drives</H2>" -body $a | Out-File $destination -Append
 
 #Get Printers
 
-Get-Printer | Select Name, Type, PortName | ConvertTo-HTML -Head "<H2 style='color:green;'>Printers</H2>" -body $a | Out-file $file -append
+Get-Printer | Select Name, Type, PortName | ConvertTo-HTML -Head "<H2 style='color:green;'>Printers</H2>" -body $a | Out-file $destination -append
 
 #Send Email
 
-$fromaddress = "<insert your email address>"
-$toaddress = "<insert your email address>"
+# $fromaddress = "<insert your email address>"
+# $toaddress = "<insert your email address>"
 $Subject = "System Report for Agent"
-$body = Get-Content $file
-$smtpserver = "<your smtp address>" #for example, smtp.office365.com
-$Password = "<insert your email password>"
-$port = <insert smtp port> #for example, 587
+$body = Get-Content $destination
+# $smtpserver = "<your smtp address>" #for example, smtp.office365.com
+# $Password = "<insert your email password>"
+# $port = <insert smtp port> #for example, 587
  
 $message = new-object System.Net.Mail.MailMessage
 $message.IsBodyHTML = $true

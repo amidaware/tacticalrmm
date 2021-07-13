a592d2b39729011cf94fcc08a313738a38dc9c72
silversword411 <github.com@datmail.com>
2021-07-13 00:21:43 -0400

    Adding scripts to library and WIP

---
+++ scripts_wip/Win_Driver_Restrict_PrinterInstallToAdmin.bat
@@ -0,0 +1,1 @@
+reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v RestrictDriverInstallationToAdministrators /t REG_DWORD /d 1 /f
---
+++ scripts_wip/Win_Hello_Disable.bat
@@ -0,0 +1,2 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\PassportForWork" /v Enabled /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\PassportForWork" /v DisablePostLogonProvisioning /t REG_DWORD /d 1 /f

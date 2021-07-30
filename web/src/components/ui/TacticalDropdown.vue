0d96129f2d8de890a9d85ab8c2b4aa98f5c7b941
sadnub <josh@torchlake.com>
2021-07-30 12:45:26 -0400

    get dropdown filtering working on custom tactical dropdown component

--- web/src/components/ui/TacticalDropdown.vue
+++ web/src/components/ui/TacticalDropdown.vue
@@ -3,23 +3,37 @@
     dense
     options-dense
     @update:model-value="value => $emit('update:modelValue', value)"
+    :options="filterable ? filteredOptions : options"
     :model-value="modelValue"
     :map-options="mapOptions"
     :emit-value="mapOptions"
     :multiple="multiple"
     :use-chips="multiple"
+    :use-input="filterable"
+    @[filterEvent]="filterFn"
   >
     <template v-slot:option="scope">
-      <q-item v-if="!scope.opt.category" v-bind="scope.itemProps" class="q-pl-lg">
+      <q-item
+        v-if="!scope.opt.category"
+        v-bind="scope.itemProps"
+        class="q-pl-lg"
+        :key="mapOptions ? scope.opt.value : null"
+      >
         <q-item-section>
           <q-item-label v-html="mapOptions ? scope.opt.label : scope.opt"></q-item-label>
         </q-item-section>
       </q-item>
-      <q-item-label v-if="scope.opt.category" header class="q-pa-sm">{{ scope.opt.category }}</q-item-label>
+      <q-item-label v-if="scope.opt.category" header class="q-pa-sm" :key="scope.opt.category">{{
+        scope.opt.category
+      }}</q-item-label>
     </template>
   </q-select>
 </template>
 <script>
+// composition imports
+import { toRefs, computed } from "vue";
+import { useDropdownFilter } from "@/composables/quasar";
+
 export default {
   name: "tactical-dropdown",
   props: {
@@ -32,6 +46,25 @@ export default {
       type: Boolean,
       default: false,
     },
+    filterable: {
+      type: Boolean,
+      default: false,
+    },
+    options: !Array,
+  },
+  setup(props) {
+    const { options } = toRefs(props);
+    const { filterFn, filteredOptions } = useDropdownFilter(options, !props.mapOptions);
+
+    const filterEvent = computed(() => {
+      return props.filterable ? "filter" : null;
+    });
+
+    return {
+      filterFn,
+      filteredOptions,
+      filterEvent,
+    };
   },
 };
 </script>

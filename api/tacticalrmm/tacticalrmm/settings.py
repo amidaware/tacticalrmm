a20b1a973eea9ddb5be5c1491a00a4a969e52709
wh1te909 <dcparsi@gmail.com>
2021-09-01 07:18:09 +0000

    bump version

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -15,14 +15,14 @@ EXE_DIR = os.path.join(BASE_DIR, "tacticalrmm/private/exe")
 AUTH_USER_MODEL = "accounts.User"
 
 # latest release
-TRMM_VERSION = "0.8.1"
+TRMM_VERSION = "0.8.2"
 
 # bump this version everytime vue code is changed
 # to alert user they need to manually refresh their browser
-APP_VER = "0.0.143"
+APP_VER = "0.0.144"
 
 # https://github.com/wh1te909/rmmagent
-LATEST_AGENT_VER = "1.6.0"
+LATEST_AGENT_VER = "1.6.1"
 
 MESH_VER = "0.9.16"
 

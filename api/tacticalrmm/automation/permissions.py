e5df566c7a6bd21d57c24be25d4d9801a3291e25
wh1te909 <dcparsi@gmail.com>
2021-11-05 05:14:15 +0000

    typo

--- api/tacticalrmm/automation/permissions.py
+++ api/tacticalrmm/automation/permissions.py
@@ -6,6 +6,6 @@ from tacticalrmm.permissions import _has_perm
 class AutomationPolicyPerms(permissions.BasePermission):
     def has_permission(self, r, view):
         if r.method == "GET":
-            return _has_perm(r, "can_list_automation_polcies")
+            return _has_perm(r, "can_list_automation_policies")
         else:
             return _has_perm(r, "can_manage_automation_policies")

2830e7c569c6ab45642d430a618f268cbac29dc7
silversword411 <github.com@datmail.com>
2021-12-16 12:57:09 -0500

    script - tweak AUOptions revert

--- scripts/Win_Windows_Update_RevertToDefault.ps1
+++ scripts/Win_Windows_Update_RevertToDefault.ps1
@@ -1,4 +1,4 @@
 # Tactical RMM Patch management disables Windows Automatic Update settings by setting the registry key below to 1. 
 # Run this to revert back to default
 
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions" -Value "0"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions"

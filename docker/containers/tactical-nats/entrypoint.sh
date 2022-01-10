a2fac5d94630f7c5969f42652bec940ac898a94e
sadnub <josh@torchlake.com>
2022-01-09 21:36:21 -0500

    changes nats file watcher to use the NATS_CONFIG variable

--- docker/containers/tactical-nats/entrypoint.sh
+++ docker/containers/tactical-nats/entrypoint.sh
@@ -23,14 +23,14 @@ config_watcher="$(cat << EOF
 while true; do
     sleep ${NATS_CONFIG_CHECK_INTERVAL};
     if [[ ! -z \${NATS_CHECK} ]]; then
-        NATS_RELOAD=\$(date -r '/opt/tactical/api/nats-rmm.conf')
+        NATS_RELOAD=\$(date -r '${NATS_CONFIG}')
         if [[ \$NATS_RELOAD == \$NATS_CHECK ]]; then
             :
         else
             nats-server --signal reload;
-            NATS_CHECK=\$(date -r '/opt/tactical/api/nats-rmm.conf');
+            NATS_CHECK=\$(date -r '${NATS_CONFIG}');
         fi
-    else NATS_CHECK=\$(date -r '/opt/tactical/api/nats-rmm.conf');
+    else NATS_CHECK=\$(date -r '${NATS_CONFIG}');
     fi
 done
 

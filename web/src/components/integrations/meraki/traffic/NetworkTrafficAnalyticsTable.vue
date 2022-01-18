840c1b69ca6e9ce76fb2d102bc0ccfa0779d1f55
subzdev <subzdev@home.com>
2022-01-18 16:55:30 +0000

    add 7200 seconds condition again

--- web/src/components/integrations/meraki/traffic/NetworkTrafficAnalyticsTable.vue
+++ web/src/components/integrations/meraki/traffic/NetworkTrafficAnalyticsTable.vue
@@ -277,7 +277,7 @@ export default {
         timespan.value.value = "t0=" + t0 + "&t1=" + t1
         timespan.value.label = date.formatDate(t0, "MMM D, YYYY @ hh:mm A") + " - " + date.formatDate(t1, "MMM D, YYYY @ hh:mm A")
 
-      } else if (timespan.value.value !== 86400 && timespan.value.value !== 604800 && timespan.value.value !== 2592000) {
+      } else if (timespan.value.value !== 7200 && timespan.value.value !== 86400 && timespan.value.value !== 604800 && timespan.value.value !== 2592000) {
         const t0 = date.formatDate(timespan.value.value, "YYYY-MM-DDT00:00:00.000Z");
         const t1 = date.formatDate(timespan.value.value, "YYYY-MM-DDT23:59:00.000Z");
         timespan.value.value = "t0=" + t0 + "&t1=" + t1

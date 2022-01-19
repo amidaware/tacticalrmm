6d698e206ac48c77d0b420c7177a59afcec95a99
subzdev <subzdev@home.com>
2022-01-19 03:01:17 +0000

    remove more unused imports

--- web/src/views/AgentIntegrations.vue
+++ web/src/views/AgentIntegrations.vue
@@ -2,8 +2,13 @@
     <div>
         <q-splitter v-model="splitterModel">
             <template v-slot:before>
-                <q-tabs v-model="integrationTab" vertical dense class="text-primary"
-                    v-for="integration in integrations">
+                <q-tabs
+                    v-model="integrationTab"
+                    vertical
+                    dense
+                    class="text-primary"
+                    v-for="integration in integrations"
+                >
                     <q-tab :name="integration.name" :label="integration.name" />
                 </q-tabs>
             </template>
@@ -11,8 +16,14 @@
                 <q-card-section class="row items-center q-py-none">
                     <div class="text-h6">{{ agent.hostname }} Integrations</div>
                 </q-card-section>
-                <q-tab-panels v-model="integrationTab" animated swipeable vertical transition-prev="jump-up"
-                    transition-next="jump-up">
+                <q-tab-panels
+                    v-model="integrationTab"
+                    animated
+                    swipeable
+                    vertical
+                    transition-prev="jump-up"
+                    transition-next="jump-up"
+                >
                     <q-tab-panel class="q-px-none" name="Bitdefender GravityZone">
                         <Bitdefender :agent="agent" />
                     </q-tab-panel>
@@ -29,91 +40,89 @@
 </template>
 
 <script>
-    import axios from "axios";
-    import { useRoute } from 'vue-router';
-    import { ref, computed, watch, onMounted } from "vue";
-    import { useQuasar, useDialogPluginComponent, date, useMeta } from "quasar";
-    import { notifySuccess, notifyError, notifyWarning } from "@/utils/notify";
-    import Bitdefender from "@/components/integrations/bitdefender/Bitdefender";
-    import SnipeIT from "@/components/integrations/snipeit/SnipeIT";
-    import AgentMeraki from "@/components/integrations/meraki/AgentMeraki";
+import axios from "axios";
+import { useRoute } from 'vue-router';
+import { ref, watch, onMounted } from "vue";
+import { useDialogPluginComponent, useMeta } from "quasar";
+import { notifyWarning } from "@/utils/notify";
+import Bitdefender from "@/components/integrations/bitdefender/Bitdefender";
+import SnipeIT from "@/components/integrations/snipeit/SnipeIT";
+import AgentMeraki from "@/components/integrations/meraki/AgentMeraki";
 
-    export default {
-        name: "AgentIntegrations",
-        emits: [...useDialogPluginComponent.emits],
-        components: { Bitdefender, SnipeIT, AgentMeraki },
+export default {
+    name: "AgentIntegrations",
+    emits: [...useDialogPluginComponent.emits],
+    components: { Bitdefender, SnipeIT, AgentMeraki },
 
-        setup(props) {
-            const { dialogRef, onDialogHide } = useDialogPluginComponent();
-            const $q = useQuasar();
-            const route = useRoute()
-            const agent = ref([])
-            const integrations = ref([])
-            const integrationTab = ref("")
+    setup(props) {
+        const { dialogRef, onDialogHide } = useDialogPluginComponent();
+        const route = useRoute()
+        const agent = ref([])
+        const integrations = ref([])
+        const integrationTab = ref("")
 
-            function getAgent() {
-                useMeta({ title: `Agent Integrations Dashboard` });
-                axios
-                    .get("agents/" + route.params.agent_id + "/")
-                    .then(r => {
-                        agent.value = r.data
-                        getIntegrations()
-                    })
-                    .catch((e) => {
-                        console.log(e)
-                    });
-            }
+        function getAgent() {
+            axios
+                .get("agents/" + route.params.agent_id + "/")
+                .then(r => {
+                    agent.value = r.data
+                    useMeta({ title: agent.value.hostname + ` Integrations Dashboard` });
+                    getIntegrations()
+                })
+                .catch((e) => {
+                    console.log(e)
+                });
+        }
 
-            function getIntegrations() {
-                axios
-                    .get("/integrations/")
-                    .then(r => {
-                        for (let integrationObj of r.data) {
-                            if (integrationObj.enabled) {
-                                integrations.value.push(integrationObj)
-                            }
+        function getIntegrations() {
+            axios
+                .get("/integrations/")
+                .then(r => {
+                    for (let integrationObj of r.data) {
+                        if (integrationObj.enabled) {
+                            integrations.value.push(integrationObj)
                         }
-                        if (integrations.value.length < 1) {
-                            notifyWarning('No Agent Integrations configured. Go to Settings > Integrations Manager')
-                        }
-                    })
-                    .catch((e) => {
-                        console.log(e)
-                    });
+                    }
+                    if (integrations.value.length < 1) {
+                        notifyWarning('No Agent Integrations configured. Go to Settings > Integrations Manager')
+                    }
+                })
+                .catch((e) => {
+                    console.log(e)
+                });
+        }
+
+        watch(integrationTab, (selection) => {
+            if (selection === 'Bitdefender GravityZone') {
+                integrationTab.value = 'Bitdefender GravityZone'
+
+            } else if (selection === 'Cisco Meraki') {
+                integrationTab.value = 'Cisco Meraki'
+
+            } else if (selection === 'Snipe-IT') {
+                integrationTab.value = 'Snipe-IT'
             }
+        })
 
-            watch(integrationTab, (selection, prevSelection) => {
-                if (selection === 'Bitdefender GravityZone') {
-                    integrationTab.value = 'Bitdefender GravityZone'
+        onMounted(() => {
+            getAgent();
+        });
 
-                } else if (selection === 'Cisco Meraki') {
-                    integrationTab.value = 'Cisco Meraki'
-
-                } else if (selection === 'Snipe-IT') {
-                    integrationTab.value = 'Snipe-IT'
-                }
-            })
-
-            onMounted(() => {
-                getAgent();
-                // getIntegrations()
-            });
-
-            return {
-                splitterModel: ref(15),
-                integrationTab,
-                agent,
-                integrations,
-                // quasar dialog
-                dialogRef,
-                onDialogHide,
-            };
-        },
-    };
+        return {
+            splitterModel: ref(15),
+            integrationTab,
+            agent,
+            integrations,
+            // quasar dialog
+            dialogRef,
+            onDialogHide,
+        };
+    },
+};
 </script>
 
 <style>
-    body {
-        overflow: scroll;
-    }
+body {
+    overflow: scroll;
+}
 </style>
--- web/src/views/ClientIntegrations.vue
+++ web/src/views/ClientIntegrations.vue
@@ -1,18 +1,30 @@
 <template>
     <q-splitter v-model="splitterModel">
         <template v-slot:before>
-            <q-tabs v-model="integrationTab" vertical dense class="text-primary" v-for="integration in integrations">
+            <q-tabs
+                v-model="integrationTab"
+                vertical
+                dense
+                class="text-primary"
+                v-for="integration in integrations"
+            >
                 <q-tab :name="integration.name" :label="integration.name" />
             </q-tabs>
         </template>
         <template v-slot:after>
             <q-card-section class="row items-center q-py-none">
-                <div class="text-h6">{{node.name}} Integrations</div>
+                <div class="text-h6">{{ node.name }} Integrations</div>
             </q-card-section>
-            <q-tab-panels v-model="integrationTab" animated swipeable vertical transition-prev="jump-up"
-                transition-next="jump-up">
+            <q-tab-panels
+                v-model="integrationTab"
+                animated
+                swipeable
+                vertical
+                transition-prev="jump-up"
+                transition-next="jump-up"
+            >
                 <q-tab-panel class="q-px-none" name="Cisco Meraki">
-                   <ClientMeraki :node="node" :integrations="integrations"/>
+                    <ClientMeraki :node="node" :integrations="integrations" />
                 </q-tab-panel>
             </q-tab-panels>
         </template>
@@ -20,87 +32,85 @@
 </template>
 
 <script>
-    import axios from "axios";
-    import { useRoute } from 'vue-router';
-    import { ref, computed, watch, onMounted } from "vue";
-    import { useQuasar, useDialogPluginComponent, date, useMeta } from "quasar";
-    import { notifySuccess, notifyError, notifyWarning } from "@/utils/notify";
-    import ClientMeraki from "@/components/integrations/meraki/ClientMeraki";
+import axios from "axios";
+import { useRoute } from 'vue-router';
+import { ref, watch, onMounted } from "vue";
+import { useDialogPluginComponent, useMeta } from "quasar";
+import { notifyWarning } from "@/utils/notify";
+import ClientMeraki from "@/components/integrations/meraki/ClientMeraki";
 
-    export default {
-        name: "ClientIntegrations",
-        emits: [...useDialogPluginComponent.emits],
-        components: { ClientMeraki },
+export default {
+    name: "ClientIntegrations",
+    emits: [...useDialogPluginComponent.emits],
+    components: { ClientMeraki },
 
-        setup(props) {
-            const { dialogRef, onDialogHide } = useDialogPluginComponent();
-            const $q = useQuasar();
-            const route = useRoute()
+    setup(props) {
+        const { dialogRef, onDialogHide } = useDialogPluginComponent();
+        const route = useRoute()
 
-            const integrationTab = ref("")
-            const node = ref([])
-            const integrations = ref([])
-            const organization = ref("")
-            const orgChoice = ref(false)
+        const integrationTab = ref("")
+        const node = ref([])
+        const integrations = ref([])
+        const orgChoice = ref(false)
 
-            function getIntegrations() {
-                useMeta({ title: `Client Integrations Dashboard` });
-                axios
-                    .get("integrations/")
-                    .then(r => {
-                        for (let integrationObj of r.data) {
-                            if (integrationObj.enabled && integrationObj.client_related) {
-                                integrations.value.push(integrationObj)
-                            }
+        function getIntegrations() {
+            axios
+                .get("integrations/")
+                .then(r => {
+                    for (let integrationObj of r.data) {
+                        if (integrationObj.enabled && integrationObj.client_related) {
+                            integrations.value.push(integrationObj)
                         }
-                        if (integrations.value.length < 1) {
-                            notifyWarning('No Client Integrations configured. Go to Settings > Integrations Manager')
-                        }
-                    })
-                    .catch((e) => {
-                        console.log(e.response.data)
-                    })
+                    }
+                    if (integrations.value.length < 1) {
+                        notifyWarning('No Client Integrations configured. Go to Settings > Integrations Manager')
+                    }
+                })
+                .catch((e) => {
+                    console.log(e.response.data)
+                })
+        }
+
+        function getClient() {
+            axios
+                .get("clients/" + route.params.client_id + "/")
+                .then(r => {
+                    node.value = r.data
+                    useMeta({ title: node.value.name + ` Integrations Dashboard` });
+                })
+                .catch((e) => {
+                    console.log(e)
+
+                })
+        }
+
+        watch(integrationTab, (selection) => {
+            if (selection === 'Cisco Meraki') {
+                integrationTab.value = 'Cisco Meraki'
             }
+        })
 
-            function getClient() {
-                axios
-                    .get("clients/" + route.params.client_id + "/")
-                    .then(r => {
-                        node.value = r.data
-                    })
-                    .catch((e) => {
-                        console.log(e)
+        onMounted(() => {
+            getIntegrations()
+            getClient();
+        });
 
-                    })
-            }
-
-            watch(integrationTab, (selection, prevSelection) => {
-                if (selection === 'Cisco Meraki') {
-                    integrationTab.value = 'Cisco Meraki'
-                }
-            })
-
-            onMounted(() => {
-                getIntegrations()
-                getClient();
-            });
-
-            return {
-                splitterModel: ref(15),
-                integrationTab,
-                integrations,
-                node,
-                orgChoice,
-                // quasar dialog
-                dialogRef,
-                onDialogHide,
-            };
-        },
-    };
+        return {
+            splitterModel: ref(15),
+            integrationTab,
+            integrations,
+            node,
+            orgChoice,
+            // quasar dialog
+            dialogRef,
+            onDialogHide,
+        };
+    },
+};
 </script>
 
 <style>
-    body {
-        overflow: scroll;
-    }
+body {
+    overflow: scroll;
+}
 </style>

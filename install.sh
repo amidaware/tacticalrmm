5682c9a5b2779567d796af3b94149b44ce39908f
sadnub <josh@torchlake.com>
2022-01-22 00:07:15 -0500

    fix install.sh

--- install.sh
+++ install.sh
@@ -237,7 +237,7 @@ sudo mkdir /rmm
 sudo chown ${USER}:${USER} /rmm
 sudo mkdir -p /var/log/celery
 sudo chown ${USER}:${USER} /var/log/celery
-git clone https://github.com/sadnub/tacticalrmm.git /rmm/
+git clone https://github.com/wh1te909/tacticalrmm.git /rmm/
 cd /rmm
 git config user.email "admin@example.com"
 git config user.name "Bob"

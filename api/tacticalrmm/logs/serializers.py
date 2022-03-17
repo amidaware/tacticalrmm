7b7bdc4e9c30e07e7852d6a32d607d4a79b75a72
sadnub <josh@torchlake.com>
2022-03-16 21:55:02 -0400

    implements #827

--- api/tacticalrmm/logs/serializers.py
+++ api/tacticalrmm/logs/serializers.py
@@ -1,11 +1,21 @@
+from clients.serializers import SiteMinimumSerializer
 from rest_framework import serializers
 
 from .models import AuditLog, DebugLog, PendingAction
 
 
 class AuditLogSerializer(serializers.ModelSerializer):
-    entry_time = serializers.SerializerMethodField(read_only=True)
+    entry_time = serializers.SerializerMethodField()
     ip_address = serializers.ReadOnlyField(source="debug_info.ip")
+    site = serializers.SerializerMethodField()
+
+    def get_site(self, obj):
+        from agents.models import Agent
+        from clients.serializers import SiteMinimumSerializer
+        if obj.agent_id and Agent.objects.filter(agent_id=obj.agent_id).exists():
+            return SiteMinimumSerializer(Agent.objects.get(agent_id=obj.agent_id).site).data
+        else:
+            return None
 
     class Meta:
         model = AuditLog

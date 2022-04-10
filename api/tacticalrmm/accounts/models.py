c02ab50a0a5674fae4576b758e837ed3a1266b25
sadnub <josh@torchlake.com>
2022-04-10 10:46:47 -0400

    clear role cache on save

--- api/tacticalrmm/accounts/models.py
+++ api/tacticalrmm/accounts/models.py
@@ -197,6 +197,12 @@ class Role(BaseAuditModel):
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs) -> None:
+
+        # delete cache on save
+        cache.delete(f"{ROLE_CACHE_PREFIX}{self.name}")
+        super(BaseAuditModel, self).save(*args, **kwargs)
+
     @staticmethod
     def serialize(role):
         # serializes the agent and returns json

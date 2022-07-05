b3573cdcf8c3e2315cb0afa6a1f8bcf6ef84a279
Kevin Ruffus <kevin.ruffus@rentanerdconsulting.com>
2022-07-04 19:05:56 -0500

    Enabling webroot cert option during install

--- default-configs/python/local_settings.py
+++ default-configs/python/local_settings.py
@@ -2,26 +2,24 @@ SECRET_KEY = "DJANGO_SEKRET"
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['api.example.com']
+ALLOWED_HOSTS = ["api.example.com"]
 
 ADMIN_URL = "ADMINURL/"
 
-CORS_ORIGIN_WHITELIST = [
-    "https://rmm.example.com"
-]
+CORS_ORIGIN_WHITELIST = ["https://rmm.example.com"]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tacticalrmm',
-        'USER': 'pgusername',
-        'PASSWORD': 'pgpw',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tacticalrmm",
+        "USER": "pgusername",
+        "PASSWORD": "pgpw",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
 MESH_USERNAME = "meshusername"
 MESH_SITE = "https://mesh.example.com"
-REDIS_HOST    = "localhost"
+REDIS_HOST = "localhost"
 ADMIN_ENABLED = True

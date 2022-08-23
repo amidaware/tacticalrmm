75d9f6a7e7389acdc2d0873538b9d54eef57c24f
wh1te909 <dcparsi@gmail.com>
2022-08-23 05:09:55 +0000

    bump versions

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -17,19 +17,19 @@ LINUX_AGENT_SCRIPT = BASE_DIR / "core" / "agent_linux.sh"
 AUTH_USER_MODEL = "accounts.User"
 
 # latest release
-TRMM_VERSION = "0.14.7-dev"
+TRMM_VERSION = "0.14.7"
 
 # https://github.com/amidaware/tacticalrmm-web
-WEB_VERSION = "0.100.9-dev"
+WEB_VERSION = "0.100.9"
 
 # bump this version everytime vue code is changed
 # to alert user they need to manually refresh their browser
-APP_VER = "0.0.169"
+APP_VER = "0.0.170"
 
 # https://github.com/amidaware/rmmagent
 LATEST_AGENT_VER = "2.3.0"
 
-MESH_VER = "1.0.70"
+MESH_VER = "1.0.72"
 
 NATS_SERVER_VER = "2.8.4"
 

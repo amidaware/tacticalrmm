1f693ca4f68d323c3afd0c6a7af5d93144c9e312
wh1te909 <dcparsi@gmail.com>
2022-09-12 06:50:55 +0000

    bump versions

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -17,7 +17,7 @@ LINUX_AGENT_SCRIPT = BASE_DIR / "core" / "agent_linux.sh"
 AUTH_USER_MODEL = "accounts.User"
 
 # latest release
-TRMM_VERSION = "0.14.8-dev"
+TRMM_VERSION = "0.14.8"
 
 # https://github.com/amidaware/tacticalrmm-web
 WEB_VERSION = "0.100.9"
@@ -27,7 +27,7 @@ WEB_VERSION = "0.100.9"
 APP_VER = "0.0.170"
 
 # https://github.com/amidaware/rmmagent
-LATEST_AGENT_VER = "2.3.0"
+LATEST_AGENT_VER = "2.3.1"
 
 MESH_VER = "1.0.85"
 

0f497257892d10bdaf5903b5fb414944e2b8aae3
wh1te909 <dcparsi@gmail.com>
2022-11-22 19:13:44 +0000

    update reqs

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -34,7 +34,7 @@ LATEST_AGENT_VER = "2.4.2"
 
 MESH_VER = "1.0.97"
 
-NATS_SERVER_VER = "2.9.6"
+NATS_SERVER_VER = "2.9.7"
 
 # for the update script, bump when need to recreate venv
 PIP_VER = "34"

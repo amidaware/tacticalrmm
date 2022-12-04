91b858bf3397acb58ff96827561f6886886a883f
wh1te909 <dcparsi@gmail.com>
2022-12-04 23:31:57 +0000

    bump versions

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -20,17 +20,17 @@ MAC_UNINSTALL = BASE_DIR / "core" / "mac_uninstall.sh"
 AUTH_USER_MODEL = "accounts.User"
 
 # latest release
-TRMM_VERSION = "0.15.4-dev"
+TRMM_VERSION = "0.15.4"
 
 # https://github.com/amidaware/tacticalrmm-web
-WEB_VERSION = "0.101.8-dev"
+WEB_VERSION = "0.101.9"
 
 # bump this version everytime vue code is changed
 # to alert user they need to manually refresh their browser
 APP_VER = "0.0.175"
 
 # https://github.com/amidaware/rmmagent
-LATEST_AGENT_VER = "2.4.3-dev"
+LATEST_AGENT_VER = "2.4.3"
 
 MESH_VER = "1.1.0"
 

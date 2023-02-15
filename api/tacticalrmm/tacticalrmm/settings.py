46d955691a767d2ecb86c7422f6e3d2db96f7a14
wh1te909 <dcparsi@gmail.com>
2023-02-15 00:15:44 +0000

    update reqs

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -20,7 +20,7 @@ MAC_UNINSTALL = BASE_DIR / "core" / "mac_uninstall.sh"
 AUTH_USER_MODEL = "accounts.User"
 
 # latest release
-TRMM_VERSION = "0.15.7"
+TRMM_VERSION = "0.15.8-dev"
 
 # https://github.com/amidaware/tacticalrmm-web
 WEB_VERSION = "0.101.13"
@@ -32,9 +32,9 @@ APP_VER = "0.0.177"
 # https://github.com/amidaware/rmmagent
 LATEST_AGENT_VER = "2.4.4"
 
-MESH_VER = "1.1.2"
+MESH_VER = "1.1.4"
 
-NATS_SERVER_VER = "2.9.11"
+NATS_SERVER_VER = "2.9.14"
 
 # for the update script, bump when need to recreate venv
 PIP_VER = "34"

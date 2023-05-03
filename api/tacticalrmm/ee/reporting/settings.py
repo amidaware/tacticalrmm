924774f52abf6e94e1c8a0f1a868d4f14b6b1bab
sadnub <josh@torchlake.com>
2023-05-02 21:48:47 -0400

    fix report asset path

--- api/tacticalrmm/ee/reporting/settings.py
+++ api/tacticalrmm/ee/reporting/settings.py
@@ -11,37 +11,12 @@ class Settings:
     def __init__(self) -> None:
         self.settings = djangosettings
 
-    # settings for trmm readonly database account
-    @property
-    def REPORTING_CONNECTION_NAME(self) -> str:
-        return getattr(self.settings, "REPORTING_DB_CONNECTION_NAME", "reporting")
-
-    @property
-    def REPORTING_DB_NAME(self) -> str:
-        return getattr(self.settings, "REPORTING_DB_NAME", "tacticalrmm")
-
-    @property
-    def REPORTING_DB_HOST(self) -> str:
-        return getattr(self.settings, "REPORTING_DB_HOST", "localhost")
-
-    @property
-    def REPORTING_DB_PORT(self) -> str:
-        return getattr(self.settings, "REPORTING_DB_PORT", "5432")
-
-    @property
-    def REPORTING_DB_USER(self) -> str:
-        return getattr(self.settings, "REPORTING_DB_USER", "reporting_user")
-
-    @property
-    def REPORTING_DB_PASSWORD(self) -> str:
-        return getattr(self.settings, "REPORTING_DB_PASSWORD", "reporting_password")
-
     @property
     def REPORTING_ASSETS_BASE_PATH(self) -> str:
         return getattr(
             self.settings,
             "REPORTING_ASSETS_BASE_PATH",
-            "/opt/tactical/reporting",
+            "/opt/tactical/reporting/assets",
         )
 
     @property

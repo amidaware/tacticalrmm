cd5e87be34be9881732e1cd93ef6d4360f78e2c1
wh1te909 <dcparsi@gmail.com>
2023-05-30 18:33:31 +0000

    update reqs

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -23,11 +23,11 @@ AUTH_USER_MODEL = "accounts.User"
 TRMM_VERSION = "0.15.12-dev"
 
 # https://github.com/amidaware/tacticalrmm-web
-WEB_VERSION = "0.101.20"
+WEB_VERSION = "0.101.21-dev"
 
 # bump this version everytime vue code is changed
 # to alert user they need to manually refresh their browser
-APP_VER = "0.0.180"
+APP_VER = "0.0.181"
 
 # https://github.com/amidaware/rmmagent
 LATEST_AGENT_VER = "2.4.8"

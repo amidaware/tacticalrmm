57f64b18c695a63c71c4d39588755f997d2d07fc
wh1te909 <dcparsi@gmail.com>
2023-06-30 20:34:12 +0000

    bump web ver [skip ci]

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -23,7 +23,7 @@ AUTH_USER_MODEL = "accounts.User"
 TRMM_VERSION = "0.15.13-dev"
 
 # https://github.com/amidaware/tacticalrmm-web
-WEB_VERSION = "0.101.22"
+WEB_VERSION = "0.101.23-dev"
 
 # bump this version everytime vue code is changed
 # to alert user they need to manually refresh their browser

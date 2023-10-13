c0b145da2462fc34fc94874d79828974cd252bec
sadnub <josh@torchlake.com>
2023-10-13 17:43:00 -0400

    add yaml extension to get the current date and also subtract/add time

--- api/tacticalrmm/ee/reporting/__init__.py
+++ api/tacticalrmm/ee/reporting/__init__.py
@@ -3,3 +3,29 @@ Copyright (c) 2023-present Amidaware Inc.
 This file is subject to the EE License Agreement.
 For details, see: https://license.tacticalrmm.com/ee
 """
+
+import yaml
+import re
+from datetime import datetime, timedelta
+
+now_regex = re.compile(
+    r"^(weeks|days|hours|minutes|seconds|microseconds)=(-?\d*)$", re.X
+)
+
+
+def construct_yaml_now(loader, node):
+    loader.construct_scalar(node)
+    match = now_regex.match(node.value)
+    now = datetime.now()
+    if match:
+        now = now + timedelta(**{match.group(1): int(match.group(2))})
+    return now
+
+
+def represent_datetime_now(dumper, data):
+    value = data.isoformat(" ")
+    return dumper.represent_scalar("!now", value)
+
+
+yaml.SafeLoader.add_constructor("!now", construct_yaml_now)
+yaml.SafeDumper.add_representer(datetime, represent_datetime_now)

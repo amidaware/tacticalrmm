e2f524ce7a0ee158174692929d32acf51223f665
wh1te909 <dcparsi@gmail.com>
2023-11-01 19:30:25 +0000

    this is a major version duh [skip ci]

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -20,7 +20,7 @@ MAC_UNINSTALL = BASE_DIR / "core" / "mac_uninstall.sh"
 AUTH_USER_MODEL = "accounts.User"
 
 # latest release
-TRMM_VERSION = "0.16.7"
+TRMM_VERSION = "0.17.0"
 
 # https://github.com/amidaware/tacticalrmm-web
 WEB_VERSION = "0.101.34"

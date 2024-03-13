7e010cdbca23f9c6e65cbdb86073f8a59a3fc9bd
wh1te909 <dcparsi@gmail.com>
2024-03-13 01:06:07 +0000

    nodesource added their installation scripts back

--- update.sh
+++ update.sh
@@ -258,12 +258,7 @@ if ! which npm >/dev/null; then
   sudo apt remove -y nodejs
   sudo rm -rf /usr/lib/node_modules
 
-  curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-  NODE_MAJOR=20
-  echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-  sudo apt update
-  sudo apt install -y gcc g++ make
-  sudo apt install -y nodejs
+  curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt-get install -y nodejs
   sudo npm install -g npm
 
   cd /meshcentral

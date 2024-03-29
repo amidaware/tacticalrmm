4aa413e6979757660a021d4e4909f15da9c57ada
wh1te909 <dcparsi@gmail.com>
2024-03-29 21:07:33 +0000

    bump version

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -21,7 +21,7 @@ MAC_UNINSTALL = BASE_DIR / "core" / "mac_uninstall.sh"
 AUTH_USER_MODEL = "accounts.User"
 
 # latest release
-TRMM_VERSION = "0.18.1-dev"
+TRMM_VERSION = "0.18.1"
 
 # https://github.com/amidaware/tacticalrmm-web
 WEB_VERSION = "0.101.43"

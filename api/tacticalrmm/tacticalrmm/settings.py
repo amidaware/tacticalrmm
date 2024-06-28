d22e1d6a245b45d7fb227ee5b142f163b0897789
wh1te909 <7434746+wh1te909@users.noreply.github.com>
2024-06-28 17:12:11 +0000

    update nats-server

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -35,7 +35,7 @@ LATEST_AGENT_VER = "2.7.0"
 
 MESH_VER = "1.1.21"
 
-NATS_SERVER_VER = "2.10.16"
+NATS_SERVER_VER = "2.10.17"
 
 # Install Nushell on the agent
 # https://github.com/nushell/nushell

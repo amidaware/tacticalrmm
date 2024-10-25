0d021a800af139e1733e48db304ad084de0b311c
wh1te909 <7434746+wh1te909@users.noreply.github.com>
2024-10-25 03:34:07 +0000

    use exists

--- api/tacticalrmm/ee/sso/permissions.py
+++ api/tacticalrmm/ee/sso/permissions.py
@@ -10,5 +10,4 @@ from allauth.socialaccount.models import SocialAccount
 
 class SSOLoginPerms(permissions.BasePermission):
     def has_permission(self, r, view):
-        connected_apps = SocialAccount.objects.filter(user=r.user)
-        return len(connected_apps) > 0
+        return SocialAccount.objects.filter(user=r.user).exists()

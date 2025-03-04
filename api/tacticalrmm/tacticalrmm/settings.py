ec2297d27db64c06cc8c1b1f259fa71091371d65
wh1te909 <7434746+wh1te909@users.noreply.github.com>
2025-03-04 02:33:15 +0000

    bump versions

--- api/tacticalrmm/tacticalrmm/settings.py
+++ api/tacticalrmm/tacticalrmm/settings.py
@@ -26,14 +26,14 @@ AUTH_USER_MODEL = "accounts.User"
 TRMM_VERSION = "0.20.2-dev"
 
 # https://github.com/amidaware/tacticalrmm-web
-WEB_VERSION = "0.101.52"
+WEB_VERSION = "0.101.53"
 
 # bump this version everytime vue code is changed
 # to alert user they need to manually refresh their browser
-APP_VER = "0.0.197"
+APP_VER = "0.0.198"
 
 # https://github.com/amidaware/rmmagent
-LATEST_AGENT_VER = "2.8.0"
+LATEST_AGENT_VER = "2.9.0"
 
 MESH_VER = "1.1.32"
 
@@ -83,7 +83,7 @@ INSTALL_DENO_URL = ""
 DENO_DEFAULT_PERMISSIONS = "--allow-all"
 
 # for the update script, bump when need to recreate venv
-PIP_VER = "45"
+PIP_VER = "46"
 
 SETUPTOOLS_VER = "75.8.0"
 WHEEL_VER = "0.45.1"
